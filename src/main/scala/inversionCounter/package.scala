396078d5b9e788c32f59012d0967285b236fd8b0
chemacortes <chemacortes@localhost>
2014-05-24 17:25:58 +0200

    Use typeclass pattern to change from view bounded Ordered to context bounded Ordering

--- src/main/scala/inversionCounter/package.scala
+++ src/main/scala/inversionCounter/package.scala
@@ -2,16 +2,16 @@ import scala.annotation.tailrec
 
 package object inversionCounter {
 
-  case class OrderedList[T <% Ordered[T]](list: List[T], numInv: BigInt) {
+  case class OrderedList[T: Ordering](list: List[T], numInv: BigInt) {
     def ++(aList: OrderedList[T]) =
       merge(list, aList.list, Nil, numInv + aList.numInv)
   }
 
   object OrderedList {
-    def apply[T <% Ordered[T]](list: List[T]) = sort(list)
+    def apply[T: Ordering](list: List[T]) = sort(list)
   }
 
-  private def sort[T <% Ordered[T]](list: List[T]): OrderedList[T] = {
+  private def sort[T: Ordering](list: List[T]): OrderedList[T] = {
     val n = list.length
     if (n == 1)
       new OrderedList(list, 0) // Base case
@@ -22,15 +22,16 @@ package object inversionCounter {
   }
 
   @tailrec
-  private def merge[T <% Ordered[T]](left: List[T],
-                                     right: List[T],
-                                     total: List[T],
-                                     numInv: BigInt): OrderedList[T] =
+  private def merge[T: Ordering](left: List[T],
+                                 right: List[T],
+                                 total: List[T],
+                                 numInv: BigInt): OrderedList[T] =
     (left, right) match {
       case (Nil, _) => new OrderedList(total ++ right, numInv)
       case (_, Nil) => new OrderedList(total ++ left, numInv)
       case (b :: btail, c :: ctail) =>
-        if (b <= c)
+        val ord = implicitly[Ordering[T]]
+        if (ord.lteq(b, c))
           merge(btail, right, total :+ b, numInv)
         else
           merge(left, ctail, total :+ c, numInv + left.length)
