f2b5f37c37fe4e6c52ac904168566a34af3df08b
chemacortes <chemacortes@localhost>
2014-05-24 17:25:58 +0200

    Use typeclass pattern to change from view bounded Ordered to context bounded Ordering

---
+++ src/main/scala/inversionCounter/package.scala
@@ -0,0 +1,40 @@
+import scala.annotation.tailrec
+
+package object inversionCounter {
+
+  case class OrderedList[T: Ordering](list: List[T], numInv: BigInt) {
+    def ++(aList: OrderedList[T]) =
+      merge(list, aList.list, Nil, numInv + aList.numInv)
+  }
+
+  object OrderedList {
+    def apply[T: Ordering](list: List[T]) = sort(list)
+  }
+
+  private def sort[T: Ordering](list: List[T]): OrderedList[T] = {
+    val n = list.length
+    if (n == 1)
+      new OrderedList(list, 0) // Base case
+    else {
+      val (left, right) = list.splitAt(n / 2)
+      sort(left) ++ sort(right)
+    }
+  }
+
+  @tailrec
+  private def merge[T: Ordering](left: List[T],
+                                 right: List[T],
+                                 total: List[T],
+                                 numInv: BigInt): OrderedList[T] =
+    (left, right) match {
+      case (Nil, _) => new OrderedList(total ++ right, numInv)
+      case (_, Nil) => new OrderedList(total ++ left, numInv)
+      case (b :: btail, c :: ctail) =>
+        val ord = implicitly[Ordering[T]]
+        if (ord.lteq(b, c))
+          merge(btail, right, total :+ b, numInv)
+        else
+          merge(left, ctail, total :+ c, numInv + left.length)
+    }
+
+}
---
+++ src/main/scala/prueba.scala
@@ -0,0 +1,19 @@
+object prueba extends App {
+
+  import scala.io.Source
+  import inversionCounter._
+
+  val l = OrderedList(List(1, 3, 4, 2, 5, 6, 8, 7))
+
+  println(l)
+
+  val s = OrderedList("Hello, world!".toList)
+  println(s)
+
+  val array = Source.fromFile("IntegerArray.txt").getLines.toList map (_.toInt)
+
+  val res = OrderedList(array)
+
+  println(res.numInv) //--> 2407905288
+
+}
